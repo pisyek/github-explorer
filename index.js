4633c8235036a9c6578f9a6be4e1ffbf73e583b3
Pisyek K <pisyek@gmail.com>
2019-05-15 23:50:05 +0800

    add octokit search github endpoint

--- index.js
+++ index.js
@@ -4,14 +4,31 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const hbs =  require('hbs');
+const octokit = require('./src/lib/octokit');
 const port = process.env.PORT;
 const publicDir = path.join(__dirname, './public');
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 
 app.use(express.static(publicDir));
 
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/search-github', async (req, res) => {
+    const repository = req.query.repository;
+    const language = req.query.language;
+    const topic = req.query.topic;
+
+    let data = await octokit.searchRepository(repository, language, topic);
+
+    res.send({
+        result: data,
+        repository,
+        language,
+        topic
+    });
+});
 
 app.get('*', (req, res) => {
     res.render('404');
