382c76ac5974e1d045d33f0016fcc3a651964271
Pisyek K <pisyek@gmail.com>
2019-05-16 21:35:23 +0800

    add test case for octokit

---
+++ test/octokitTest.js
@@ -0,0 +1,11 @@
+const octokit = require('../src/lib/octokit');
+
+const { assert } = require('chai');
+
+describe('Octokit', function() {
+    it('Github search repo to get 1 result.', async function() {
+        let result = await octokit.searchRepository('laravel', null, null, 1);
+        let itemCount = result.data.items.length;
+        assert.equal(itemCount, 1);
+    }).timeout(5000);
+});
