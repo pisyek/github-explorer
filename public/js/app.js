00f7026cbef023cd6e8f4619f052a7de4f518cbf
Pisyek K <pisyek@gmail.com>
2019-05-18 19:04:25 +0800

    remove console log)

--- public/js/app.js
+++ public/js/app.js
@@ -39,7 +39,6 @@ $searchForm.addEventListener('submit', (e) => {
                 ]
             });
             $searchButton.removeAttribute('disabled');
-            console.log(data);
         });
     });
 });
