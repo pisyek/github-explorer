013ba31e745c299718d8953f71659bf714d0df8b
Pisyek K <pisyek@gmail.com>
2019-05-18 23:38:55 +0800

    change db string connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const {DB_HOST, DB_PORT, DB_NAME} = process.env;
 
-const database = 'mongodb://'+DB_HOST+':'+DB_PORT+'/'+DB_NAME;
+const database = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 mongoose.connect(database, {
     useNewUrlParser: true,
     useCreateIndex: true,
