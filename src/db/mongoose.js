b989e6f2e26c22abe47e3f4a7c70a0f4fc3252bf
Pisyek K <pisyek@gmail.com>
2019-05-20 12:30:54 +0800

    update env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const {DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, DB_ADMIN} = process.env;
+const {DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, DB_AUTH} = process.env;
 
 const database = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 mongoose.connect(database, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    authSource: "admin"
+    authSource: DB_AUTH
 });
